612d1c2a6912634290c7a8379993e093c0ed623c
unknown <ssm0232@nepes.co.kr>
2026-04-27 16:41:10 +0900

    [FEAT] remove radio block for compatible with ble

--- test.ts
+++ test.ts
@@ -1,4 +1,3 @@
-robodog.setMode(deflib.RobodogMode.Uart)
 robodog.gesture(deflib.Posture.Ready)
 robodog.legBend(deflib.LegGroup.AllLegs, 60)
 robodog.leg(deflib.LegSelection.FrontLegs, 60, 0)
@@ -11,25 +10,14 @@ robodog.headLedPrint(deflib.HeadLedSide.Both, "A")
 robodog.bodyLed(0, 128, 255)
 robodog.soundPlay(deflib.SoundEffect.Bark, deflib.SoundVolume.Medium)
 
-robodog.setMode(deflib.RobodogMode.Radio)
-robodog.rfBand(7)
-robodog.aiDetection(deflib.AiMode.FaceDetect)
-robodog.faceTracking(deflib.AiClass.Class1)
-let cameraReady = robodog.getCameraAlive()
-let faceClass = robodog.getFaceClass()
-let qrValue = robodog.getQrCode()
-let aiX = robodog.getAiPosition(deflib.AiPositionAxis.X)
-
 basic.forever(function () {
     let battery = robodog.getBattery()
     let distance = robodog.getTof()
     let tilt = robodog.getTilt(deflib.TiltAxis.ForwardBackward)
     let rotation = robodog.getRotation()
 
-    if (cameraReady && robodog.getButton()) {
+    if (robodog.getButton()) {
         basic.showNumber(battery)
-    } else if (faceClass > 0 || aiX != 0 || qrValue != "none") {
-        robodog.headLedExp(deflib.LedExpression.SparklingEyes)
     } else if (distance < 20) {
         robodog.move(deflib.MoveDirection.Forward, 0)
     } else if (tilt > 20 || rotation > 180) {
